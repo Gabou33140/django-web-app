d8f9853e0ad238cc3e28e2f2bd9a2e6617488374
godillar <gabriel.odillard@outlook.fr>
2023-07-27 15:21:17 +0200

    maj git ignore , ajout models/vues/url

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Band(models.model):
+    name = models.fields.CharField(max_length=100)
+
+
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,10 +1,38 @@
 # ~/projects/django-web-app/merchex/listings/views.py
 
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
+from listings.models import Band, Listing
+
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    band = Band.objects.all()
+    return HttpResponse(f"""
+            <h1>Hello Django!</h1>
+            <p> Voici 3 groupes : <p>
+            <ul>
+                <li>{band[0]}</li>
+                <li>{band[1]}</li>
+                <li>{band[2]}</li>
+            </ul>
+            """)
+
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+
+def contact(request):
+    return HttpResponse('<h1>Contacte nous:</h1>')
+
+
+def listing(request):
+    listing = Listing.objects.all()
+    return HttpResponse(f"""
+            <h1>Voici la liste:</h1>
+            <ul>
+                <li>{listing[0]}</li>
+                <li>{listing[1]}</li>
+                <li>{listing[2]}</li>
+            </ul>
+            """)
