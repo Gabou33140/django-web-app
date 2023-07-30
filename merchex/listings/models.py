ded8941aaecb66d50ff7926d1d3b120b3a4d0d5a
godillar <gabriel.odillard@outlook.fr>
2023-07-31 01:30:10 +0200

    migrations , fonctionnalités admin

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class Band(models.Model):
+    def __str__(self):
+        return f"{self.name}"
+    
     class Genre(models.TextChoices):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
@@ -27,4 +31,5 @@ class Listing(models.Model):
     sold = models.fields.BooleanField(default=False)
     year = models.fields.IntegerField(validators=[MinValueValidator(2000)], null=True)
     type = models.CharField(choices=Type.choices, max_length=20, null=False, default="Autres")
-
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+    description_bis = models.fields.CharField(max_length=100, null=False, default='Pas de description')
