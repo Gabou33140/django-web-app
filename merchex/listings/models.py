4b517baaa5b917616794c1f77b7c883bbfe79edf
godillar <gabriel.odillard@outlook.fr>
2023-07-31 01:46:39 +0200

    migrations , fonctionnalités admin

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -32,4 +32,3 @@ class Listing(models.Model):
     year = models.fields.IntegerField(validators=[MinValueValidator(2000)], null=True)
     type = models.CharField(choices=Type.choices, max_length=20, null=False, default="Autres")
     band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
-    description_bis = models.fields.CharField(max_length=100, null=False, default='Pas de description')
